9475836c2c715e28037d606fdb57ca288fef6ffd
errorhandler404 <errorhandler404@gmail.com>
2026-01-10 05:54:39 +0000

    model file - write metadata pydantic code

---
+++ model/models.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, Field
+from typing import Optional, List, Dict, Any
+
+
+class Metadata(BaseModel):
+    Summary: list[str] = Field(default_factory=list, description="Summary of document")
+    Title: str
+    Author: str
+    DateCreated: str
+    LastModifiedDate: str
+    Publisher: str
+    Language: str
+    PageCount: Union[int, str]
+    SentimentTone: str
