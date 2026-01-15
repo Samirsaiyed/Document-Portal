3c9d0f6eaefcf7943cf09cc0e51b5e56f3100011
errorhandler404 <errorhandler404@gmail.com>
2026-01-15 10:51:41 +0000

    Data ingestion code structure is ready - Data Compare

---
+++ src/document_compare/data_ingestion.py
@@ -0,0 +1,30 @@
+import os, sys
+from pathlib import Path
+import fitz
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+
+class DocumentComparator:
+
+    def __init__(self):
+        pass
+
+    def delete_existing_files(self):
+        """
+        Delete existing files at the specified paths
+        """        
+        pass
+
+    def save_uploaded_file(self):
+        """
+        Save uploaded file to a specified directory. 
+        """
+        pass
+
+    def read_pdf(self):
+        """
+        Read the pdf file and extract text from each page.
+        """
+        pass
+    
+
