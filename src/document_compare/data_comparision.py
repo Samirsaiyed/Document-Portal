51ec17da907bfd481a8bb1c5ec8983092c1d19e1
errorhandler404 <errorhandler404@gmail.com>
2026-01-15 11:11:38 +0000

    Code skeleton is ready - Doc comparision

---
+++ src/document_compare/data_comparision.py
@@ -0,0 +1,28 @@
+import sys
+from dotenv import load_dotenv
+import pandas as pd
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+from prompt.prompt_libraries import PROMPT_REGISTRY
+from utils.model_loader import ModelLoader
+
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.output_parsers import OutputFixingParser
+
+
+class DocumentComparatorLLM:
+
+    def __init__(self):
+        pass
+
+    def compare_documents(self):
+        """
+        Compare two documents and returns a structured comparision
+        """
+        pass
+
+    def _format_response(self):
+        """
+        Formats the reponse from the LLM into a structured format.
+        """
+        pass
